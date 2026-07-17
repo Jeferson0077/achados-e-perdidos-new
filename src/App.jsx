035ca6588c22063e8a999e5402ce13f3d683acc3
Jeferson Rodrigues <jeferson.rodriguess007@gmail.com>
2026-07-17 00:29:48 -0300

    fix: corrige imports das páginas administrativas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import AppRoutes from "./routes/AppRoutes"
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
+import Login from "./pages/admin/Login";
+import Dashboard from "./pages/admin/Dashboard";
+import NewItem from "./pages/admin/NewItem";
+import ActiveItems from "./pages/admin/ActiveItems";
+import WithdrawnItems from "./pages/admin/WithdrawnItems";
+import Donations from "./pages/admin/Donations";
 
 function App() {
-  return <AppRoutes />
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/admin/dashboard" element={<Dashboard />} />
+      <Route path="/admin/novo-item" element={<NewItem />} />
+      <Route path="/admin/itens-ativos" element={<ActiveItems />} />
+      <Route path="/admin/itens-retirados" element={<WithdrawnItems />} />
+      <Route path="/admin/doacoes" element={<Donations />} />
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
